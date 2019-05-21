4ea5bfb1da488bb429e77aaadceabb8c1a068881
matt_moe <mrobertmoe@gmail.com>
2019-05-20 21:36:09 -0700

    Created validations for contact form

--- javascript/validate.js
+++ javascript/validate.js
@@ -3,27 +3,49 @@ function validateForm() {
   let email = document.getElementById('email').value;
   let message = document.getElementById('message').value;
   
+  let validName = document.getElementsByClassName('valid-name');
+  let validEmail = document.getElementsByClassName('valid-email');
+  let validMsg = document.getElementsByClassName('valid-message');
+  
   if (name === "") {
     console.log('name cannot be empty');
-    return false;
+    validName[0].className = 'valid-name invalid';
+    validName[0].innerHTML = 'Name cannot be empty!';
+  } else {
+    validName[0].className = 'valid-name valid';
+    validName[0].innerHTML = 'Looks Good!';
   }
+
   if (message === "") {
     console.log('message cannot be empty');
-    return false;
-  } 
-  if (email === "") {
-    console.log('email cannot be empty');
-    return false;
-  } else {
-    let expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!expression.test(String(email).toLowerCase())) {
-      console.log('Email wrong format?');
-      return false;
-    }
+    validMsg[0].className = 'valid-message invalid';
+    validMsg[0].innerHTML = ' Message cannot be empty!';
+  }  else {
+    validMsg[0].className = 'valid-message valid';
+    validMsg[0].innerHTML = 'Looks Good!';
   }
 
-  document.getElementById('contact-form').submit();
+  if (email === "") {
+    validEmail[0].className = 'valid-email invalid';
+    validEmail[0].innerHTML = 'Email cannot be empty!';
+  } else {
+     let expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!expression.test(String(email).toLowerCase())) {
+      validEmail[0].className = 'valid-email invalid';
+      validEmail[0].innerHTML = 'Email format may be wrong';
+    }
+    validEmail[0].className = 'valid-email valid';
+    validEmail[0].innerHTML = 'Looks Good!';
+  }
+  if (wasValid()) {
+    document.getElementById('contact-form').submit();
+  }
+}
 
-  
-
+function wasValid() {
+  if (document.getElementsByClassName('invalid').length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
